75175942b1abc553d688c5fcda806082dcb407f7
vchecherynda <checherinda.vadym@gmail.com>
2026-05-14 18:44:28 +0300

    Add auth routes

---
+++ apps/api/src/routes/auth.ts
@@ -0,0 +1,81 @@
+import type { FastifyInstance } from "fastify";
+import bcrypt from "bcrypt";
+import { prisma } from "../lib/prisma.js";
+import { z } from "zod";
+
+const RegisterSchema = z.object({
+  email: z.email(),
+  password: z.string().min(8),
+});
+
+const LoginSchema = z.object({
+  email: z.email(),
+  password: z.string(),
+});
+
+export async function authRoutes(fastify: FastifyInstance) {
+  fastify.post("/auth/register", async (req, reply) => {
+    const result = RegisterSchema.safeParse(req.body);
+
+    if (!result.success) {
+      return reply.status(400).send({
+        error: "Validation failed",
+        details: z.flattenError(result.error),
+      });
+    }
+
+    const { email, password } = result.data;
+
+    const existing = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (existing) {
+      return reply.status(409).send({
+        error: "Email already registered",
+      });
+    }
+
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    const user = prisma.user.create({
+      data: { email, passwordHash },
+      select: { id: true, email: true, createdAt: true },
+    });
+
+    return reply.status(201).send({ user });
+  });
+
+  fastify.post("/auth/login", async (req, reply) => {
+    const result = LoginSchema.safeParse(req.body);
+
+    if (!result.success) {
+      return reply.status(400).send({
+        error: "Validation failed",
+        details: z.flattenError(result.error),
+      });
+    }
+
+    const { email, password } = result.data;
+
+    const user = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    // Avoiding timing attack
+    const passwordHash =
+      user?.passwordHash ?? "$2b$10$invalidhashfortimingprotection";
+    const isValid = bcrypt.compare(password, passwordHash);
+
+    if (!user || !isValid) {
+      return reply.status(401).send({
+        error: "Invalid credetials",
+      });
+    }
+
+    return reply.status(200).send({
+      user: { id: user.id, email: user.email },
+      token: "JWT token will be added soon",
+    });
+  });
+}
--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -3,6 +3,7 @@ import cors from "@fastify/cors";
 import websocket from "@fastify/websocket";
 import { startSimulation } from "./lib/simulation.js";
 import { droneRoutes } from "./routes/drones.js";
+import { authRoutes } from "./routes/auth.js";
 import { wsRoutes, broadcastDrones } from "./routes/ws.js";
 
 // TEMP: Generate fake data
@@ -25,6 +26,7 @@ await app.register(cors, { origin: "http://localhost:3000" });
 await app.register(websocket);
 
 await app.register(wsRoutes);
+await app.register(authRoutes);
 await app.register(droneRoutes);
 
 app.listen({ port: 4000 }).then(() => console.log("API on :4000"));
