acb8a2bb93b2150776d5195db9b9a7284e821434
vchecherynda <checherinda.vadym@gmail.com>
2026-05-19 21:01:32 +0300

    Add log for SIGTERM

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -63,5 +63,8 @@ const shutdown = async (signal: string) => {
   process.exit(0);
 };
 
-process.on("SIGTERM", () => shutdown("SIGTERM"));
+process.on("SIGTERM", () => {
+  console.log(">>> SIGTERM RECEIVED <<<");
+  shutdown("SIGTERM");
+});
 process.on("SIGINT", () => shutdown("SIGINT"));
