90c5e09e14d12a382b1fd07b55364344f512e1ee
vchecherynda <checherinda.vadym@gmail.com>
2026-07-02 14:22:24 +0300

    fix(shared): add missing auth import

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -2,6 +2,7 @@ export * from "./geometry.js";
 export * from "./drone.js";
 export * from "./mission.js";
 export * from "./telemetry.js";
+export * from "./auth.js";
 export * from "./reasons.js";
 export * from "./commands.js";
 export * from "./mission-results.js";
